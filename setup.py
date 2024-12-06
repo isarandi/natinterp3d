cd451b1ec867907e9f58d7d9f926275ec53ec5f7
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2024-12-06 02:31:37 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import numpy as np
+from distutils.core import setup, Extension
+
+ext_modules = [
+    Extension(
+        "natinterp3d.natinterp3d_cython",
+        sources=["natinterp3d/natinterp3d_cython.pyx", 'natinterp3d/natural.c',
+                 'natinterp3d/delaunay.c', 'natinterp3d/utils.c', 'natinterp3d/kdtree.c'],
+        include_dirs=[np.get_include(), 'natinterp3d'],
+        extra_compile_args=["-std=c11", "-O3", "-fopenmp", "-DUSE_LIST_NODE_ALLOCATOR" , '-lpthread'],
+        extra_link_args=["-fopenmp", '-lpthread']
+    )
+]
+setup(ext_modules=ext_modules)
