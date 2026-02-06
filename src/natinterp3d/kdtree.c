a7810fad3024f54fae7940601828eeeab4d7a0fa
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2026-02-07 00:08:46 +0100

    Fix Windows build: alloca.h and sys/time.h portability
    - kdtree.c: use malloc.h + _alloca on MSVC instead of alloca.h
    - predicates.c: remove unused sys/time.h include

--- src/natinterp3d/kdtree.c
+++ src/natinterp3d/kdtree.c
@@ -34,13 +34,15 @@ OF SUCH DAMAGE.
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <alloca.h>
-#include "kdtree.h"
 
-
-#if defined(WIN32) || defined(__WIN32__)
+#if defined(_MSC_VER) || defined(WIN32) || defined(__WIN32__)
 #include <malloc.h>
+#define alloca _alloca
+#else
+#include <alloca.h>
 #endif
+
+#include "kdtree.h"
 
 #ifdef USE_LIST_NODE_ALLOCATOR
 
